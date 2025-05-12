ffdfae844cfff6b906afb2e5c8b37ec6efe7f21f
«movavok» <pkurishchenkoukr.net05@gmail.com>
2025-05-12 16:57:26 +0300

    static library was created to display the main menu interface and work with the main functions

---
+++ lab11/prj/interface/interface.h
@@ -0,0 +1,8 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+#include "..\Modules_Kurishchenko_ShowAndSearch\modules_kurishchenko_showandsearch.h"
+
+void handleUserChoice(); // Основна функція для вибору операцій
+
+#endif // INTERFACE_H
